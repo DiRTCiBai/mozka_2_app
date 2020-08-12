c0d67d048b381e46bb525167ea7cd3f7319ec2ed
Sam Beyens <sam.beyens@hotmail.com>
2020-08-12 20:29:37 +0200

    scherm aanwezigheden afgemaakt

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'screens/list_swimmer_screen.dart';
 import 'screens/add_swimmer_screen.dart';
 import 'screens/swimmer_personal_data_screen.dart';
 import 'screens/aanwezigheden_screen.dart';
+import 'screens/start_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: AanwezighedenScreen.id,
+      initialRoute: StartScreen.id,
       routes: {
         SwimmersList.id: (context) => SwimmersList(),
         AddSwimmers.id: (context) => AddSwimmers(),
         SwimmerPersonalDataScreen.id: (context) => SwimmerPersonalDataScreen(),
         AanwezighedenScreen.id: (context) => AanwezighedenScreen(),
+        StartScreen.id: (context) => StartScreen(),
       },
     );
   }
--- lib/screens/aanwezigheden_screen.dart
+++ lib/screens/aanwezigheden_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mozka_2_app/modules/swimmer_data.dart';
 import 'package:mozka_2_app/screens/add_swimmer_screen.dart';
@@ -13,33 +14,126 @@ class AanwezighedenScreen extends StatefulWidget {
 }
 
 class _AanwezighedenScreenState extends State<AanwezighedenScreen> {
+  int _currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.blue,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.check),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Expanded(child: StreamBuilderListViewAanwezigheden()),
-              FlatButton(
-                color: Colors.white,
-                child: Text('done'),
-                onPressed: () {
-                  print('save en ga terug naar start scherm');
-                },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(right: 20, left: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        'Aanwezigheden',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 98,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                            top: 3, bottom: 3, left: 6, right: 6),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.group),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text('20'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(right: 10.0, left: 10.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFDFDFDF),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(45.0),
+                      ),
+                    ),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(Icons.search),
+                            hintText: 'zoek zwemmer')),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                ],
               ),
-              FlatButton(
+            ),
+            Container(
+              height: 600.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
                 color: Colors.white,
-                child: Text('cancel'),
-                onPressed: () {
-                  print('ga terug naar start scherm');
-                },
               ),
-            ],
-          ),
+              child: Container(
+                padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+                child: StreamBuilderListViewAanwezigheden(),
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        fixedColor: Colors.teal,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Search"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Add"),
+            icon: Icon(Icons.add_box),
+          ),
+        ],
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+
+            if (_currentIndex == 0) {
+              Navigator.pop(context);
+            }
+          });
+        },
       ),
     );
   }
